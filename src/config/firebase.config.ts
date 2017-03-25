0555f8a5ec5ab3814a0fc63d7cd11da0f6392fd6
drewjex <drewjex28+1@gmail.com>
2017-03-25 14:15:07 -0600

    database directory

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDNK2lQke-U05v9u_RN5DyPNeHkhBEE-sY',
+  authDomain: 'reactive-476b0.firebaseapp.com',
+  databaseURL: 'https://reactive-476b0.firebaseio.com',
+  storageBucket: 'reactive-476b0.appspot.com',
+  messagingSenderId: '51775670930'
+}
